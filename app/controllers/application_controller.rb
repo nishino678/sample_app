a70ae2741438809360d63d9d6c6190c12c3ab662
Wim F <nishino678@gmail.com>
2016-11-30 09:48:52 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-  	render html: "test heroku sample_app"
-  end
+include SessionsHelper
 end
